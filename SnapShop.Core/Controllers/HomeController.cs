bc7eba5d804b3bb6840317830e45f6dc5b1b59d6
abanoub Saweris <abanoubphelopos12@gmail.com>
2024-10-15 13:34:38 +0300

    add sidebar to the project

--- SnapShop.Core/Controllers/HomeController.cs
+++ SnapShop.Core/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using SnapShop.Core.Models;
 using SnapShop.Utility;
 
 namespace SnapShop.Core.Controllers;
@@ -9,18 +6,11 @@ namespace SnapShop.Core.Controllers;
 [Authorize]
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
     public IActionResult Index()
     {
         if (User.IsInRole(StaticDetails.RoleUserStorekeeper))
         {
-            return RedirectToAction("Index", "Category");
+            return RedirectToAction("Index", "StoreKeeper");
         }else if (User.IsInRole(StaticDetails.RoleUserCashier))
         {
             return RedirectToAction("Index", "Cashier");
@@ -29,16 +19,5 @@ public class HomeController : Controller
         {
             return RedirectToAction("Index", "Manager");
         }
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
