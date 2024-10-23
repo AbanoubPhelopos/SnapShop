fb6bb5dea71d642de5a0a8b152c46ee3be5cce5f
abanoub Saweris <abanoubphelopos12@gmail.com>
2024-10-23 22:04:41 +0300

    toggle the lock button

---
+++ SnapShop.Application/ViewModel/UserVM.cs
@@ -0,0 +1,9 @@
+﻿namespace SnapShop.Application.ViewModel;
+
+public class UserVM
+{
+    public string Id { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+    public bool IsLocked { get; set; }
+}
