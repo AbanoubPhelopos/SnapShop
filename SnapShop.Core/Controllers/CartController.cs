5b6449a4369c8eff6495813abaed18713f400e2e
abanoub Saweris <abanoubphelopos12@gmail.com>
2024-10-25 20:34:28 +0300

    checkout bug

--- SnapShop.Core/Controllers/CartController.cs
+++ SnapShop.Core/Controllers/CartController.cs
@@ -271,7 +271,7 @@ namespace SnapShop.Core.Controllers
 			}
 
 			// Create a new Stripe Checkout session
-			var domain = "http://localhost:5081/"; // Update with your domain
+			var domain = "https://snapshop2024.azurewebsites.net/"; // Update with your domain
 			var options = new SessionCreateOptions
 			{
 				SuccessUrl = $"{domain}Cart/Confirmation",
